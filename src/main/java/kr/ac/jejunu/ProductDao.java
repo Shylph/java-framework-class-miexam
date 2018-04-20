7523359a17a20d87746a5d4c0483d1a5d9238abd
myks790 <myks790@gmail.com>
2018-04-20 16:29:06 +0900

    상품 수정, 삭제 기능 추가

--- src/main/java/kr/ac/jejunu/ProductDao.java
+++ src/main/java/kr/ac/jejunu/ProductDao.java
@@ -3,18 +3,18 @@ package kr.ac.jejunu;
 import javax.sql.DataSource;
 import java.sql.*;
 
-public class ProductDao  {
+public class ProductDao {
     private final DataSource dataSource;
 
-    public ProductDao(DataSource dataSource){
-        this.dataSource =dataSource;
+    public ProductDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public Product get(Long id) throws SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-        Product product;
+        Product product = null;
         try {
             connection = dataSource.getConnection();
 
@@ -22,12 +22,12 @@ public class ProductDao  {
             preparedStatement.setLong(1, id);
 
             resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-
-            product = new Product();
-            product.setId(resultSet.getLong("id"));
-            product.setTitle(resultSet.getString("title"));
-            product.setPrice(resultSet.getInt("price"));
+            if (resultSet.next()) {
+                product = new Product();
+                product.setId(resultSet.getLong("id"));
+                product.setTitle(resultSet.getString("title"));
+                product.setPrice(resultSet.getInt("price"));
+            }
         } finally {
             //자원을 해지한다.
             if (resultSet != null) {
@@ -73,7 +73,7 @@ public class ProductDao  {
             resultSet = preparedStatement.getGeneratedKeys();
             resultSet.next();
             id = resultSet.getLong(1);
-        }  finally {
+        } finally {
             //자원을 해지한다.
             if (resultSet != null) {
                 try {
@@ -100,6 +100,69 @@ public class ProductDao  {
         }
 
         return id;
+    }
+
+    public void delete(Long id) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+
+            preparedStatement = connection.prepareStatement("delete from product where id=?");
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+
+    public void update(Product product) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+
+            preparedStatement = connection.prepareStatement("update product set title=?, price=? where id=?");
+            preparedStatement.setString(1, product.getTitle());
+            preparedStatement.setLong(2, product.getPrice());
+            preparedStatement.setLong(3, product.getId());
+
+            preparedStatement.executeUpdate();
+        } finally {
+            //자원을 해지한다.
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
     }
 }
 
